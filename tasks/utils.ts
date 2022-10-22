12e95f659bff9224a6347fbcbda8abde1eac3ca8
sowell-owen <karakulin.toshik@gmail.com>
2022-10-23 00:02:30 +0300

    feat: deleted preAction

--- tasks/utils.ts
+++
@@ -1,4 +0,0 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-
-export const preAction = (hre: HardhatRuntimeEnvironment): Promise<void> =>
-  hre.run("clean").then(() => hre.run("compile").then(() => console.log("")));
